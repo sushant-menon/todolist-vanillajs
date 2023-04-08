5bcfc5cbab821ec4f0e2726a16b71f689d5320f6
Sushant <sushantnambiar@gmail.com>
2023-04-09 00:28:56 +0530

    add items to list added

--- script.js
+++ script.js
@@ -12,6 +12,32 @@ function addItem(e) {
     alert("Input field empty , add item!!!");
     return;
   }
+
+  //Create list items
+
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
 }
 
 // Event Listener
