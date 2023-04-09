3e293a15d49ed562ae808d7e1862d1ed031dae28
Sushant <sushantnambiar@gmail.com>
2023-04-10 02:44:46 +0530

    added search function

--- script.js
+++ script.js
@@ -74,9 +74,25 @@ function checkUI() {
   }
 }
 
+function searchItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach(item => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 // Event Listener
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearButton.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", searchItems);
 
 checkUI();
